98fdf5b86377b5a961d798575badb7a8b434d374
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-07 12:08:04 +0200

    Fix: fix todo display

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -5,9 +5,15 @@ function App() {
   return (
     <main className="py-10 h-screen bg-gray-800">
       <h1 className="font-bold text-3xl text-center text-gray-400">Your Todos</h1>
-      <div>{todos.map(todo => (
-        <p>{todo.title}</p>
-      ))}</div>
+      <div className="max-w-lg mx-auto">
+        <div className="space-y-2">
+          {todos.map(todo => (
+            <p key={todo.id} className="text-lg text-gray-200">
+              {todo.title}
+            </p>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
