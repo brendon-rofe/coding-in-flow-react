71cd365fa9f9c0acaecfff647ecc04d377ff5fe1
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-07 11:51:28 +0200

    feat: More setup

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -2,10 +2,12 @@
 function App() {
 
   return (
-    <main className="py-10">
-      <h1>Your Todos</h1>
+    <main className="py-10 h-screen bg-gray-800">
+      <h1 className="font-bold text-3xl text-center text-gray-400">
+        Your Todos
+      </h1>
     </main>
-  )
+  );
 }
 
 export default App
