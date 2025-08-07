259d52077f721599c20de1ae17e09d8cf70dc049
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-07 12:01:58 +0200

    feat: List todo titles

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -1,9 +1,13 @@
+import { todos } from "./data/todos";
 
 function App() {
 
   return (
     <main className="py-10 h-screen bg-gray-800">
       <h1 className="font-bold text-3xl text-center text-gray-400">Your Todos</h1>
+      <div>{todos.map(todo => (
+        <p>{todo.title}</p>
+      ))}</div>
     </main>
   );
 }
