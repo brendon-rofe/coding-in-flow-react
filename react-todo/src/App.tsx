c3b0c9a3b004705d71c06a818dba6dcdf3c321a0
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-07 11:59:04 +0200

    feat: Create initial todos and Todo type

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -3,9 +3,7 @@ function App() {
 
   return (
     <main className="py-10 h-screen bg-gray-800">
-      <h1 className="font-bold text-3xl text-center text-gray-400">
-        Your Todos
-      </h1>
+      <h1 className="font-bold text-3xl text-center text-gray-400">Your Todos</h1>
     </main>
   );
 }
---
+++ react-todo/src/data/todos.ts
@@ -0,0 +1,7 @@
+import type { Todo } from "../types/todo";
+
+export const todos: Todo[] = [
+  { id: 1, title: "Buy groceries", completed: false },
+  { id: 2, title: "Finish React project", completed: true },
+  { id: 3, title: "Go to the gym", completed: false },
+];
