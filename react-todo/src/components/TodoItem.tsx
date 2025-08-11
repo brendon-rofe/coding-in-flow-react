ebcf0becbb2f30b653a2fb231860ecfb3c5a5e0d
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-11 15:54:40 +0200

    fix: Line-through

--- react-todo/src/components/TodoItem.tsx
+++ react-todo/src/components/TodoItem.tsx
@@ -12,7 +12,7 @@ export default function TodoItem ({todo}: TodoItemProps) {
         <span
           className="w-5 h-5 rounded-md border-2 border-gray-400 peer-checked:bg-gray-500 peer-checked:border-black-500 transition-colors duration-200"
         ></span>
-        <span className={todo.completed ? "linethrough text-gray-400" : ""}>{todo.title}</span>
+        <span className={todo.completed ? "line-through text-gray-400" : ""}>{todo.title}</span>
       </label>
     </div>
   )
