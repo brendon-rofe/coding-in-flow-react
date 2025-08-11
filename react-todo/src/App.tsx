7a527e6e7bef799f1f1f894f02bb149bb9b05e2e
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-11 15:25:51 +0200

    feat: Update styling

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -4,9 +4,9 @@ import { todos } from "./data/todos";
 function App() {
 
   return (
-    <main className="py-10 h-screen bg-gray-800">
-      <h1 className="font-bold text-3xl text-center text-gray-400">Your Todos</h1>
-      <div className="max-w-lg mx-auto">
+    <main className="py-10 h-screen space-y-5">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className="space-y-2">
           {todos.map(todo => (
             <TodoItem todo={todo}/>
--- react-todo/src/components/TodoItem.tsx
+++ react-todo/src/components/TodoItem.tsx
@@ -7,8 +7,11 @@ interface TodoItemProps {
 export default function TodoItem ({todo}: TodoItemProps) {
   return (
     <div>
-      <label>
-        <input type="checkbox" className="scale-125" />
+      <label className="flex items-center gap-2 border rounded-md p-2 hover:bg-slate-50">
+        <input type="checkbox" className="sr-only peer" />
+        <span
+          className="w-5 h-5 rounded-md border-2 border-gray-400 peer-checked:bg-gray-500 peer-checked:border-black-500 transition-colors duration-200"
+        ></span>
         <span className={todo.completed ? "linethrough text-gray-400" : ""}>{todo.title}</span>
       </label>
     </div>
