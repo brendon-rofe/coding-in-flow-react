947b56c44c166412ab4388184a2f066c5d966e01
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-12 16:50:14 +0200

    feat: Create check callback

--- react-todo/src/components/TodoItem.tsx
+++ react-todo/src/components/TodoItem.tsx
@@ -2,13 +2,19 @@ import type { Todo } from "../types/todo";
 
 interface TodoItemProps {
   todo: Todo;
+  onCompletedChange: (id: number, completed: boolean) => void;
 }
 
-export default function TodoItem ({todo}: TodoItemProps) {
+export default function TodoItem ({todo, onCompletedChange}: TodoItemProps) {
   return (
     <div>
       <label className="flex items-center gap-2 border rounded-md p-2 hover:bg-slate-50">
-        <input type="checkbox" className="sr-only peer" />
+        <input 
+          type="checkbox" 
+          checked={todo.completed} 
+          onChange={(e) => onCompletedChange(todo.id, e.target.checked)} 
+          className="sr-only peer" 
+        />
         <span
           className="w-5 h-5 rounded-md border-2 border-gray-400 peer-checked:bg-gray-500 peer-checked:border-black-500 transition-colors duration-200"
         ></span>
