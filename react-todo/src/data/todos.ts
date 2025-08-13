cc4581d90cc99cd8569e259f0fd4d60583bed2e3
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-08-13 10:40:34 +0200

    feat: Update

--- react-todo/src/data/todos.ts
+++ react-todo/src/data/todos.ts
@@ -1,6 +1,6 @@
 import type { Todo } from "../types/todo";
 
-export const todos: Todo[] = [
+export const dummyData: Todo[] = [
   { id: 1, title: "Buy groceries", completed: false },
   { id: 2, title: "Finish React project", completed: false },
   { id: 3, title: "Go to the gym", completed: false },
